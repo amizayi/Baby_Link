9783768229383fb695e6ebb2ea16ec3ac8d56ed5
Amirreza <hjrezi1999@gmail.com>
2023-01-01 19:32:39 +0330

    api response trait

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+trait ApiResponse
+{
+    //success response api
+    protected function successResponse($data, $message = null, $code = 200)
+    {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'result' => $data
+        ], $code);
+    }
+    //error response api
+    protected function errorResponse($message = null, $code = 400)
+    {
+        return response()->json(['status' => false, 'message' => $message], $code);
+    }
+    //error response validation api
+    protected function validationResponse($message, $code = 403)
+    {
+        return throw (new HttpResponseException(response()->json(['status' => false, 'message' => $message], $code)));
+    }
+}
