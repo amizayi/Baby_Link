80233353d12321881603a8aa5525eab43ebda8ae
Amirreza <hjrezi1999@gmail.com>
2023-01-02 09:02:52 +0330

    make resource for show link

---
+++ app/Http/Resources/V1/Link/LinkResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\V1\Link;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LinkResource extends JsonResource
+{ 
+    public function toArray($request)
+    {
+        return [
+            'code' => $this->code,
+            'redirect' => $this->redirect_url,
+            'type' => $this->type?->name,
+            'status' => $this->status?->name,
+            'created_at' => jdate($this->created_at)->ago(),
+        ];
+    }
+}
--- app/Models/Link.php
+++ app/Models/Link.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Base\Status;
+use App\Models\Base\Type;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
@@ -10,5 +12,17 @@ class Link extends Model
 {
     use HasFactory, SoftDeletes;
 
+    protected $table = 'links';
+
     protected $guarded = ['id'];
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
