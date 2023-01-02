8f984b48a4dda063ce6d52b45ae62ba583f37742
Amirreza <hjrezi1999@gmail.com>
2023-01-02 09:00:00 +0330

    fix query on find code (change statusId)

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/{code}', function ($code) {
     //find public code
     $redirect = Link::query()
         ->whereCode($code)
-        ->whereStausId(1)
+        ->whereStatusId(1)
         ->select('redirect_url')
         ->first()?->redirect_url;
     // Does not exist code
